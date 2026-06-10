a79446f161e0db510c5cfe187b2db75a821c4fce
GGodson <olg.osipov777@gmail.com>
2026-06-10 23:23:26 +0300

    Ignore Fast Refresh warnings for shadcn UI primitives

--- eslint.config.js
+++ eslint.config.js
@@ -19,7 +19,16 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  {
+    files: ['src/components/ui/**/*.tsx'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
     },
   },
 );
