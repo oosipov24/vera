ee4c9900ff471ba1e35521037c9baf81e3f6289e
GGodson <olg.osipov777@gmail.com>
2026-06-11 00:06:59 +0300

    Add route constants

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 
 import { TradingPage } from '@/pages/trading';
+import { ROUTES } from '@/shared/config/routes';
 
 export function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/trading" replace />} />
-        <Route path="/trading" element={<TradingPage />} />
+        <Route path={ROUTES.root} element={<Navigate to={ROUTES.trading} replace />} />
+        <Route path={ROUTES.trading} element={<TradingPage />} />
+        <Route path="*" element={<Navigate to={ROUTES.trading} replace />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/shared/config/routes.ts
@@ -0,0 +1,4 @@
+export const ROUTES = {
+  root: '/',
+  trading: '/trading',
+} as const;
